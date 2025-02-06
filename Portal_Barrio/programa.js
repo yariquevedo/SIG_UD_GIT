f61d59e443a04b29c1e122cbbba4c6cfd04b929f
Yaritza Quevedo <yquevedo59@gmail.com>
2025-02-05 20:31:02 -0500

    Barrio_0.2

---
+++ Portal_Barrio/programa.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.616688555906419, -74.15938380177191], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
